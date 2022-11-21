df5d3fdbae8e73716d72bccd6aec537eac8135ae
Giorgi Mtsituri <giorgimtsituri42@gmail.com>
2022-11-21 09:33:11 +0400

    My first React file

---
+++ script.js
@@ -0,0 +1,20 @@
+const navbar = (
+    <nav>
+        <h1>Giorgi's page</h1> 
+        <ul>
+            <li>Menu</li>
+            <li>Contact</li>
+            <li>About</li>
+
+        </ul>
+
+
+    </nav>
+   
+)
+
+
+
+
+
+ReactDOM.render(navbar,document.querySelector(".root"))
